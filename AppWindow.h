071e616d52013e5d5b06174220975d219edaeb35
Setiel B. Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2022-09-18 22:26:54 +0800

    tutorial 7 update

--- AppWindow.h
+++ AppWindow.h
@@ -4,20 +4,19 @@
 #include "SwapChain.h"
 #include "DeviceContext.h"
 #include "VertexBuffer.h"
-
+#include "VertexShader.h"
 class AppWindow : public Window
 {
+public:
+	AppWindow();
+	~AppWindow();
 
-	public:
-		AppWindow();
-		~AppWindow();
-
-		// Inherited via Window
-		virtual void onCreate() override;
-		virtual void onUpdate() override;
-		virtual void onDestroy() override;
-
-	private:
-		SwapChain* m_swap_chain;
-		VertexBuffer* m_vb;
+	// Inherited via Window
+	virtual void onCreate() override;
+	virtual void onUpdate() override;
+	virtual void onDestroy() override;
+private:
+	SwapChain* m_swap_chain;
+	VertexBuffer* m_vb;
+	VertexShader* m_vs;
 };
--- DeviceContext.h
+++ DeviceContext.h
@@ -3,6 +3,7 @@
 
 class SwapChain;
 class VertexBuffer;
+class VertexShader;
 
 class DeviceContext
 {
@@ -14,6 +15,8 @@ public:
 	void drawTriangleStrip(UINT vertex_count, UINT start_vertex_index);
 
 	void setViewportSize(UINT width, UINT height);
+
+	void setVertexShader(VertexShader* vertex_shader);
 
 	bool release();
 	~DeviceContext();
